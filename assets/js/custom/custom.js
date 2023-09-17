fc32483f300fbaa56d6b2152eeb768e4a7d6c4ee
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2023-09-17 11:46:09 +0900

    문의하기의 hidden 파라메터 수정

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -739,7 +739,7 @@ function sendApplicationData(form_id, token)
 	$(form_id).append(ref);	
 	ref = $('<input type="hidden" value="' + min_type + '" name="min_type">');	
 	$(form_id).append(ref);	
-	ref = $('<input type="hidden" value="gpscontact" name="form_kind">');	
+	ref = $('<input type="hidden" value="catchmecontact" name="form_kind">');	
 	$(form_id).append(ref);
 
 	if (isRecaptchaInit == false) {
